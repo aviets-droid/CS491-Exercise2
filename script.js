93882aa0aa40498a9e0574f43bd4294a567283e2
aviets-droid <aviets@unr.edu>
2025-06-14 13:09:28 -0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-// function createTable(num_rows, num_cols) {
-//     //
-// }
+function createTable(num_rows, num_cols) {
+    for (var cr=1; cr<=num_rows; cr++) {
+        document.write("<tr>")
+        for (var cc=1; cc<=num_cols; cc++) {
+            document.write("<td>X</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
